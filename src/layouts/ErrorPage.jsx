ea3992e3c31b1211a68fc930fea32eff546b3f79
niloykhan002 <barkatificbankbd@gmail.com>
2025-01-11 19:58:22 +0600

    error page added

--- src/layouts/ErrorPage.jsx
+++ src/layouts/ErrorPage.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h2>Error happened</h2>
+    <div className="min-h-screen flex justify-center items-center bg-rare">
+      <div className="flex flex-col gap-6 items-center">
+        <h1 className="text-7xl font-extrabold">
+          <span className="text-primary">Oo</span>ps!
+        </h1>
+        <h2 className="text-xl">404 Page Not Found</h2>
+        <Link to={"/"} className="btn bg-primary text-white">
+          Go Home
+        </Link>
+      </div>
     </div>
   );
 };
